4e1092872284c8a5ae51b6d8e9c7557d5a193a24
Mikhail <sadsnake@mixbytes.io>
2020-03-03 15:22:30 +0300

    Remove `RawEvent`

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,8 @@ decl_event!(
     where
         AccountId = <T as system::Trait>::AccountId,
         TableId = <T as Trait>::TableId,
-        TargetType = <T as Trait>::TargetType, {
+        TargetType = <T as Trait>::TargetType
+    {
         TableCreated(TableId, AccountId),
         ChangeVote(TableId, TargetType),
     }
@@ -84,7 +85,7 @@ decl_module! {
         pub fn create_table(origin, vote_asset: AssetId<T>, head_len: u8, name: Option<Vec<u8>>) -> dispatch::DispatchResult {
             let who = ensure_signed(origin)?;
             let id = Self::create(who.clone(), vote_asset, head_len, name)?;
-            Self::deposit_event(RawEvent::TableCreated(id, who));
+            Self::deposit_event(Event::<T>::TableCreated(id, who));
 
             Ok(())
         }
@@ -95,7 +96,7 @@ decl_module! {
             let table = Scores::<T>::get(table_id);
             assets::Module::<T>::reserve(&table.vote_asset, &who, vote)?;
 
-            Self::deposit_event(RawEvent::ChangeVote(table_id, target.clone()));
+            Self::deposit_event(Event::<T>::ChangeVote(table_id, target.clone()));
 
             match Scores::<T>::mutate(&table_id, |table| table.vote(target, &who, vote)) {
                 VoteResult::Success(Some(reward)) => Self::send_reward(&table.vote_asset, &table.wallet, &who, reward),
@@ -109,7 +110,7 @@ decl_module! {
             let who = ensure_signed(origin)?;
             let table = Scores::<T>::get(table_id);
 
-            Self::deposit_event(RawEvent::ChangeVote(table_id, target.clone()));
+            Self::deposit_event(Event::<T>::ChangeVote(table_id, target.clone()));
 
             match Scores::<T>::mutate(&table_id, |table| table.unvote(target, &who, vote)) {
                 VoteResult::Unvoted(unvote, reward) => {
@@ -170,9 +171,12 @@ decl_module! {
 }
 
 impl<T: Trait> Module<T> {
-
-    pub fn create(who: AccountId<T>, vote_asset: AssetId<T>, head_len: u8, name: Option<Vec<u8>>) -> Result<T::TableId, Error<T>>
-    {
+    pub fn create(
+        who: AccountId<T>,
+        vote_asset: AssetId<T>,
+        head_len: u8,
+        name: Option<Vec<u8>>,
+    ) -> Result<T::TableId, Error<T>> {
         let id = Self::get_next_table_id()?;
         Scores::<T>::insert(id, Table::<T>::new(name, head_len, vote_asset, who)); // ToDo create normal wallet
         Ok(id)
